bb2ce674f4d5d8573c30175d707a0f0b0fbc2035
Natalia <natikrak@gmail.com>
2020-04-02 10:15:55 +0200

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (id_book,title,author_name,author_last_name) VALUES (1, 'Ziemia obiecana', 'Wladyslaw','Reymond');
+INSERT INTO book (id_book,title,author_name,author_last_name) VALUES (2, 'Lalka', 'Boleslaw','Prus');
+
