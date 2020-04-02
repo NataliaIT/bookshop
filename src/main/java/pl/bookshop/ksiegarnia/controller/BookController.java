ecfbda175b52013628dd5cc7cc23e5225543e5ff
Natalia <natikrak@gmail.com>
2020-04-02 11:00:10 +0200

    list

--- src/main/java/pl/bookshop/ksiegarnia/controller/BookController.java
+++ src/main/java/pl/bookshop/ksiegarnia/controller/BookController.java
@@ -1,11 +1,15 @@
 package pl.bookshop.ksiegarnia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.bookshop.ksiegarnia.entity.Book;
 import pl.bookshop.ksiegarnia.repository.BookRepository;
+
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -23,4 +27,10 @@ public class BookController {
         return bookRepository.findById(id).get();
     }
 
+
+    @GetMapping("/book/list")
+    public ResponseEntity<List<Book>> showAllBooks(){
+        List<Book> books = bookRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(books);
+    }
 }
